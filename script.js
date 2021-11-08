28492ca3945934ebf8392a2047ab8a8b911fd53c
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2021-11-08 02:11:22 +0100

    Esercizio finito

---
+++ script.js
@@ -0,0 +1,31 @@
+// dati utente
+let km = parseFloat(prompt('numero di chilometri da percorrere'));
+let age = parseInt(prompt('Inserisci la tua età'));
+
+// prezzo biglietto senza sconti
+let kmPrice = km * 0.21;
+
+// calcolo sconti
+if(isNaN(age) || isNaN(km))
+{
+    alert('Errore inserimento dati, ricaricare la pagina.')
+}
+else
+{
+    if(age < 18)
+    {
+        price = (kmPrice * 20 / 100) - kmPrice; // devo dichiarare price?
+    }
+    else if(age > 65)
+    {
+        price = (kmPrice * 20 / 100) - kmPrice;
+    }
+    else
+    {
+        price = kmPrice;
+    }
+}
+
+// rendo visibile il prezzo del biglietto
+let result = Math.abs(price.toFixed(2)) + ' ' + 'euro';
+document.getElementById('result').innerHTML = result;
